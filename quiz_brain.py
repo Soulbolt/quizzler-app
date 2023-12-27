5512bb4fac41799bcedc720916a7123a2b04f9c4
Soulbolt <soulbolt@gmail.com>
2023-12-27 13:11:32 -0700

    refactored some code, implemented functions for true and false button clicks. code allows UI to provide feedback if answer is right or wrong along with score update of correct answers.

--- quiz_brain.py
+++ quiz_brain.py
@@ -6,24 +6,21 @@ class QuizBrain:
         self.score = 0
         self.question_number = 0
         self.question_list = question_list
+        self.current_question = self.question_list[self.question_number]
 
     def still_has_questions(self):
-        if len(self.question_list) > self.question_number:
-            return True
+        return len(self.question_list) > self.question_number
 
     def next_question(self):
-        current_question = self.question_list[self.question_number]
+        self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        q_text = html.unescape(current_question.text)
-        return f"Q.{self.question_number}: {html.unescape(q_text)}"
-        # user_answer = input(f"Q.{self.question_number}: {html.unescape(current_question.text)} (True/False)\n").lower()
-        # self.check_answer(user_answer, current_question.answer)
+        q_text = html.unescape(self.current_question.text)
+        return f"Q.{self.question_number}: {q_text}"
 
-    def check_answer(self, user_answer, correct_answer):
+    def check_answer(self, user_answer):
+        correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
-            print("You got it right!")
+            return True
         else:
-            print("That's wrong.")
-        print(f"The correct answer was: {correct_answer}.")
-        print(f"Your current score is: {self.score}/{self.question_number}\n")
+            return False
