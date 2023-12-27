26d321731c279912256e10a3c590740713e443c8
Soulbolt <soulbolt@gmail.com>
2023-12-27 10:54:30 -0700

    Created class for UI, implemented base UI and a WIP function for true button click. Commented out some unncessary code in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 # Create a variable to hold list of questions and answers
 question_bank = []
@@ -12,11 +13,12 @@ for question in question_data:
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface()
 
 # Check if quiz still has questions remaining
-while quiz.still_has_questions():
-    # Ask user the questions
-    quiz.next_question()
+# while quiz.still_has_questions():
+#     # Ask user the questions
+#     quiz.next_question()
 
 print("You've completed the quiz!")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- ui.py
+++ ui.py
@@ -1,2 +1,27 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+        self.canvas = Canvas(width=300, height=250)
+        self.canvas.create_text(150, 150, text="place holder", font=("Ariel", 20, "italic"))
+        self.canvas.grid(column=0, row=1, columnspan=2)
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(column=1, row=0)
+        self.false_image = PhotoImage(file="./images/false.png")
+        self.false_button = Button(image=self.false_image)
+        self.false_button.grid(column=0, row=2)
+        self.true_image = PhotoImage(file="./images/true.png")
+        self.true_button = Button(image=self.true_image, command=self.true_button_click)
+        self.true_button.grid(column=1, row=2)
+
+        self.window.mainloop()
+
+    def true_button_click(self):
+        return "true"
+        
